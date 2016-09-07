c5556648f1d51406960a1d87814515b10b93e96c
Eric <erictaw2018@u.northwestern.edu>
2016-09-07 11:17:02 -0500

    equation labelling capable

--- katex_display_math_module.js
+++ katex_display_math_module.js
@@ -13,14 +13,14 @@ module.exports = function(md) {
 
         // check if it's a one-line expression
         var firstLine = state.src.slice(pos, max)
-        
+
         if(firstLine !== '$$' && firstLine.trim().slice(-2) === '$$'){
             var latex = firstLine.trim().slice(2, -2)
             state.line += 1
         }
 
         // not a one-line expression, so look for the next $$
-            
+
         else {
             for (var nextLine = startLine + 1; nextLine <= endLine; nextLine++) {
                 pos = state.bMarks[nextLine] + state.tShift[nextLine]
@@ -51,8 +51,17 @@ module.exports = function(md) {
         if (tokens[idx].content === "") {
             return '<code>Nothing was rendered for this LaTeX equation. Perhaps your markdown syntax is wrong?</code>'
         }
-        return katex.renderToString(tokens[idx].content, {
-            displayMode: true
-        })
+        return '<div class="container-fluid">'
+            + '<div class="col-sm-8 col-sm-offset-2">'
+            + katex.renderToString(tokens[idx].content, {
+                displayMode: true
+            })
+            + '</div>'
+            + '<div class="col-sm-2">'
+            + '<p class="equation-label">'
+            + '(1)'
+            + '</p>'
+            + '</div>'
+            + '</div>'
     }
 }
